4e5f9be1d4802aaed5b3596b9ec006cc630bf590
Ankit Kumar <heyankitkr@gmail.com>
2022-08-08 10:40:27 +0530

    image fix

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,41 +1,40 @@
-import React, {useState, useEffect} from 'react'
-import { getProducts } from './helper/coreapicalls'
+import React, { useState, useEffect } from "react";
+import { getProducts } from "./helper/coreapicalls";
 
-import Base from './Base';
+import Base from "./base";
 import "../styles.css";
-import Card from './Card';
+import Card from "./card";
 
 export default function Home() {
+	const [products, setProducts] = useState([]);
+	const [error, setError] = useState(false);
 
-  const [products, setProducts] = useState([]);
-  const [error, setError] = useState(false);
+	const loadAllProducts = () => {
+		getProducts().then((data) => {
+			if (data.error) {
+				console.log(error);
+			} else {
+				setProducts(data);
+			}
+		});
+	};
 
-  const loadAllProducts = () => {
-        getProducts().then((data) => {
-          if (data.error) {
-            console.log(error);
-          }else{
-            setProducts(data);
-          }
-        });
-  };
+	useEffect(() => {
+		loadAllProducts();
+	}, []);
 
-  useEffect(() => {
-    loadAllProducts();
-  }, []);
-
-  return (
-    <Base title = "Home Page" description="This is T-shirt Store">
-      <h1>Home Component</h1>
-      <div className='row'>
-          {products.map((products, index) =>{
-            return(
-                <div key={index} className="col-4 mb-4">
-                    <Card />
-                </div>
-            )
-          })}
-      </div>
-    </Base>
-  );
+	return (
+		<Base title="Home Page" description="This is T-shirt Store">
+			<h1>Home Component</h1>
+			<div className="row">
+				{products.map((product, index) => {
+					return (
+						<div key={index} className="col-4 mb-4">
+							<Card product={product} />
+						</div>
+					);
+				})}
+			</div>
+		</Base>
+	);
 }
