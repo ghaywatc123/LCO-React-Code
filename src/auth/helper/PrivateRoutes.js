a54fb04b33e65dd6abe79972a231195a607fb7ab
Chetan <ghaywat01@gmail.com>
2022-08-23 14:45:52 +0530

    23/08/22

--- src/auth/helper/PrivateRoutes.js
+++ src/auth/helper/PrivateRoutes.js
@@ -1,29 +1,29 @@
-import React, { Component } from "react"
-import {Routes,  Navigate, Outlet} from "react-router-dom"
+import React from "react"
+import {Routes,  Navigate, Outlet, Component} from "react-router-dom"
 import { isAuthenticated } from "."
 
 
 
 const PrivateRoutes = ({ component: Component, ...rest }) => {
-    return (
-      <Routes
-        {...rest}
-        render={(props) =>
-          isAuthenticated()
-            ? (
-              <Component {...props} />
-            )
-            : (
-              <Navigate
-                to={{
-                  pathname: "/signin",
-                  state: { from: props.location },
-                }}
-              />
-            )}
-      />
-    );
-  return isAuthenticated() ? <Outlet /> : <Navigate to={"/signin"} />;
-  };
+  return (
+    <Routes
+      {...rest}
+      render={(props) =>
+        isAuthenticated()
+          ? (
+            <Component {...props} />
+          )
+          : (
+            <Navigate
+              to={{
+                pathname: "/signin",
+                state: { from: props.location },
+              }}
+            />
+          )}
+    />
+  );
+return isAuthenticated() ? <Outlet /> : <Navigate to="/signin" />;
+};
 
-export default PrivateRoutes
+export default PrivateRoutes 
